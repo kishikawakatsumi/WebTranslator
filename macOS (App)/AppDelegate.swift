0c59811834152b3790e6f5dbcd93dd9e90823195
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2022-10-07 04:47:38 +0900

    Refactor

--- macOS (App)/AppDelegate.swift
+++ macOS (App)/AppDelegate.swift
@@ -1,21 +1,8 @@
-//
-//  AppDelegate.swift
-//  macOS (App)
-//
-//  Created by kishikawa katsumi on 2022/09/20.
-//
-
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Override point for customization after application launch.
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
-
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
 }
