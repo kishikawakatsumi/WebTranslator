d26d18a4f7af52e4c229b7e7b343380ec86fce0f
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2022-10-12 15:22:45 +0900

    Fix toast message glitch

--- Shared (Extension)/src/content/toast.js
+++ Shared (Extension)/src/content/toast.js
@@ -13,9 +13,7 @@ export class Toast {
       const group = createToastGroup();
 
       group.addToast(`${progressMessage()}...`, { autoDismiss: -1 });
-      setTimeout(() => {
-        retouchToastAppearance();
-      }, 10);
+      retouchToastAppearance();
     }
     this._snackbarStack.push(true);
   }
@@ -59,21 +57,23 @@ function retouchToastAppearance() {
   if (group) {
     const toast = group.querySelector("nord-toast");
     if (toast) {
-      toast.innerHTML = `<nord-stack direction="horizontal" gap="s" align-items="center"><svg width="24" height="24" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"><animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite"/></path></svg><div style="color: #fff;">${progressMessage()}...</div></nord-stack>`;
+      toast.innerHTML = `<nord-stack direction="horizontal" gap="m" align-items="center"><svg width="24" height="24" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"><animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite"/></path></svg><div style="font-size: 14px; color: #fff;">${progressMessage()}...</div></nord-stack>`;
       const shadowRoot = toast.shadowRoot;
       if (shadowRoot) {
-        const toastInner = shadowRoot.querySelector(".n-toast");
-        if (toastInner) {
-          const slot = toastInner.querySelector("slot");
-          const dismissButton = toastInner.querySelector(".n-dismiss");
-          if (dismissButton) {
-            dismissButton.style.visibility = "hidden";
+        requestAnimationFrame(() => {
+          const toastInner = shadowRoot.querySelector(".n-toast");
+          if (toastInner) {
+            const dismissButton = toastInner.querySelector(".n-dismiss");
+            if (dismissButton) {
+              dismissButton.style.visibility = "hidden";
+            }
           }
-        }
+        });
       }
     }
   }
 }
+
 function dismissToast() {
   let success = false;
   const group = document.querySelector("nord-toast-group");
