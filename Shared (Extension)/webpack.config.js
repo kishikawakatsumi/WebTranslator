ded3f7638b5214f1a44d8329aaf65971a816d585
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-05-19 01:27:44 +0900

    0.1.20

--- Shared (Extension)/webpack.config.js
+++ Shared (Extension)/webpack.config.js
@@ -27,9 +27,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
-          },
         },
       },
       {
